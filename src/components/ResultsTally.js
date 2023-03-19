1e010240923d9d90739c47974afb9ee9d6dddddb
Liz Burton <liz.burton147@gmail.com>
2023-03-19 09:25:38 -0400

    add results tally

--- src/components/ResultsTally.js
+++ src/components/ResultsTally.js
@@ -1,5 +1,28 @@
-function ResultsTally() {
-  return <div></div>;
-}
+import React from "react";
+import { Modal } from "semantic-ui-react";
+import ResponseTable from "./ResponseTable";
+
+const ResultsTally = ({
+  gameOver,
+  numQuestions,
+  onModalClose,
+  questionList,
+  mode,
+}) => {
+  return (
+    <Modal open={gameOver.open} onClose={onModalClose}>
+      <Modal.Header>
+        <h2>
+          {gameOver.congrat} You got {gameOver.total} out of {numQuestions}{" "}
+          correct!
+        </h2>
+      </Modal.Header>
+      <Modal.Content>
+        <ResponseTable questionList={questionList} mode={mode} />
+        <button onClick={onModalClose}>DONE</button>
+      </Modal.Content>
+    </Modal>
+  );
+};
 
 export default ResultsTally;
